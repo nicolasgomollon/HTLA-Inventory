d548f340ebe2e6adac6420954b80d21322eed18b
Nicolas Gomollon <nicolasgomollon@techno-magic.com>
2012-06-08 12:37:55 -0700

    Initializes rails stuff.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HTLAInventory::Application.config.secret_token = 'b272d7eb35b76ce4eaeb7b95216ebc99f7dd65fced9231c30dd1d4e9eb89086ceb404cfc36f7649de2b432516d8b7b075d14a3734c758afa064647f49da7e258'
