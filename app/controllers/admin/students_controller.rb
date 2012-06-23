f19e61486bad6157c9059dc098697e23f6fb9d38
Julian Moore <jreedmoore@gmail.com>
2012-06-22 23:51:34 -0700

    Adds search to admin/students#index

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,7 +1,28 @@
 
 class Admin::StudentsController < ApplicationController
   def index
-    @students = Student.all
+    query = ""
+    query_array = []
+    query = query_helper 'firstname', params[:firstname], query, query_array
+    query = query_helper 'lastname', params[:lastname], query, query_array
+    query = query_helper 'studentid', params[:id], query, query_array
+    
+    @students = Student.where(query, *query_array) unless query.empty?
+    
+    @first = params[:firstname]
+    @last = params[:lastname]
+    @id = params[:id]
+  end
+  
+  def query_helper name, value, query_string, query_array
+    if !value.nil? then
+      if !value.empty? then
+        query_string += " OR " unless query_string.empty?
+        query_string += "#{name} LIKE ?"
+        query_array.push(value)
+      end
+    end
+    query_string
   end
   
   def show
