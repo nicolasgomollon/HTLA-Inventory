32f4b1d4be80128aa8c47c032a307e200738e2b5
Julian Moore <jreedmoore@gmail.com>
2012-06-26 14:56:46 -0700

    Functional tests for StudentsController

--- test/functional/admin/students_controller_test.rb
+++ test/functional/admin/students_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class Admin::StudentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "routing" do
+    assert_routing "/admin/students", {:controller => "admin/students", :action => "index"} 
+  end
+  
+  test "search" do
+    get :index, { :firstname => students(:julian).firstname }
+    assert_equal assigns(:students)[0], students(:julian)
+    
+    get :index, { :firstname => students(:julian).firstname, :id => students(:julian).id }
+    assert_equal assigns(:students)[0], students(:julian)
+    
+    get :index, { :id => students(:julian).id }
+    assert_equal assigns(:students)[0], students(:julian)
+    
+    get :index, { :firstname => students(:bobby).firstname }
+    assert_equal assigns(:students)[0], students(:bobby)
+  end
+  
+  test "show" do
+    get :show, {:id => students(:julian).id}
+    assert_response :success
+    assert_equal assigns(:student), students(:julian)
+  end
 end
