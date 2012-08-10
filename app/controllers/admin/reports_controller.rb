5d8e22393055c4b1dc2534c63cbe0d1c7afcdebf
Julian Moore <jreedmoore@gmail.com>
2012-08-09 21:01:24 -0700

    DRY's up reports controller and ugly repetitive views
    Resolves #19

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -8,55 +8,39 @@ class Admin::ReportsController < Admin::AdminController
 	end
 
 	def bad
-		@bad = []
+		@list = []
+		@report_name = "Bad"
 		comp_location.includes(:computer_parts).all.each do |computer|
-			@bad << computer if computer.bad?
+			@list << computer if computer.bad?
 		end
-		respond_to do |format|
-			format.html
-			format.csv {
-				render :csv => @bad, :filename => location_name("Bad")
-			}
-		end
+		do_respond
 	end
 
 	def good
-		@good = []
+		@list = []
+		@report_name = "Good"
 		comp_location.includes(:computer_parts).all.each do |computer|
-			@good << computer if computer.good?
+			@list << computer if computer.good?
 		end
-		respond_to do |format|
-			format.html
-			format.csv {
-				render :csv => @good, :filename => location_name("Good")
-			}
-		end
+		do_respond
 	end
 
 	def assigned
-		@assigned = []
+		@list = []
+		@report_name = "Assigned Computers"
 		comp_location.includes(:computer_ownerships).all.each do |computer| 
-			@assigned << computer unless computer.get_current_student.nil?
+			@list << computer unless computer.get_current_student.nil?
 		end
-		respond_to do |format|
-			format.html
-			format.csv {
-				render :csv => @assigned, :filename => location_name("Assigned Computers")
-			}
-		end
+		do_respond
 	end
 
 	def checkout
-		@checkout = []
+		@list = []
+		@report_name = "Unassigned Checkout Computers"
 		comp_location.includes(:computer_ownerships).all.each do |computer| 
-			@checkout << computer if computer.get_current_student.nil?
+			@list << computer if computer.get_current_student.nil?
 		end
-		respond_to do |format|
-			format.html
-			format.csv {
-				render :csv => @checkout, :filename => location_name("Checkout Computers")
-			}
-		end
+		do_respond
 	end
 
 	def parts
@@ -75,17 +59,13 @@ class Admin::ReportsController < Admin::AdminController
 		@bad = params[:commit] == 'bad'
 		@part = params[:part]
 		@list = []
+		@report_name = location_name((@bad ? "Bad" : "Good") + " " + @part)
 		comp_location.includes(:computer_parts).all.each do |computer|
 			unless computer.parts[@part].nil? then
 				@list << computer if computer.parts[@part].status != @bad
 			end
 		end
-		respond_to do |format|
-			format.html
-			format.csv {
-				render :csv => @list, :filename => location_name((@bad ? "Bad" : "Good") + " " + @part)
-			}
-		end
+		do_respond
 	end
 
 	def fullcsv
@@ -96,18 +76,38 @@ class Admin::ReportsController < Admin::AdminController
 	end
 
 	def comp_location
-		unless params[:location].nil? then
-			return Location.find(params[:location]).computers
+		unless get_location.nil? then
+			return @location.computers
 		else
 			return Computer
 		end
 	end
 
+	def get_location
+		return @location unless @location.nil?
+		unless params[:location].nil? then
+			@location = Location.find(params[:location]) 
+			return @location
+		end
+		return nil
+	end
+
 	def location_name(name)
-		unless @location.nil? then
+		unless get_location.nil? then
 			return name + " from #{@location.name}"
 		else
 			return name
+		end
+	end
+
+	def do_respond
+		respond_to do |format|
+			format.html {
+				render 'report_view'
+			}
+			format.csv {
+				render :csv => @list, :filename => location_name(@report_name)
+			}
 		end
 	end
 end
